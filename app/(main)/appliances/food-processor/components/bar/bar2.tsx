6a5c0470aac750c8020a105a5edc1af1acd45558
Delight007 <ganalafiyalevi@gmail.com>
2025-06-22 18:19:28 +0100

    error fix

--- app/(main)/appliances/food-processor/components/bar/bar2.tsx
+++ app/(main)/appliances/food-processor/components/bar/bar2.tsx
@@ -1,7 +1,7 @@
+import PriceRange2 from "@/src/components/phones-tables/allProducts/bar/PriceRange/price";
 import { Fugaz_One } from "next/font/google";
 import Link from "next/link";
 import { FaPaperPlane } from "react-icons/fa";
-import PriceRange from "../../../allProducts/mProduct/bar/PriceRange/priceRange";
 import FoodProcessorBrandsMenu from "../foodProcessorBrand";
 
 const fugazOne = Fugaz_One({ subsets: ["latin"], weight: ["400"] });
@@ -27,7 +27,7 @@ export default function FoodprocessorBar2() {
       </div>
 
       <div>
-        <PriceRange />
+        <PriceRange2 />
       </div>
     </div>
   );
